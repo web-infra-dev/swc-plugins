ad12ad2e331a53f80be650189c7879a46991dfd9
jerrykingxyz <jerrykingxyz@gmail.com>
2022-10-24 18:45:06 +0800

    feat: keep swc version to 1.3.4

--- crates/shared/src/lib.rs
+++ crates/shared/src/lib.rs
@@ -3,7 +3,7 @@ pub mod utils;
 // reexports some same version libs
 pub use anyhow;
 pub use serde_json;
-pub use swc_ecma_transforms_testing;
-pub use testing;
 pub use swc_core;
+pub use swc_core::ecma::transforms::testing as swc_ecma_transforms_testing;
+pub use testing;
 pub extern crate serde;
