3a5627304cbce2ebc27b77dfbf9f1113d57efe94
GiveMe-A-Name <a455300764b@hotmail.com>
2023-04-04 16:07:38 +0800

    fix: update test snapshot

--- crates/plugin_react_utils/tests/fixtures/remove_prop_types/shared-prop-type-variable/expected-remove.js
+++ crates/plugin_react_utils/tests/fixtures/remove_prop_types/shared-prop-type-variable/expected-remove.js
@@ -1,4 +1,3 @@
-import _extends from "@swc/helpers/src/_extends.mjs";
 import React from 'react';
 import { omit } from 'underscore';
 import Bar from './bar';
@@ -8,5 +7,5 @@ const propTypes = {
 };
 export default function Foo(props) {
   const barProps = omit(props, Object.keys(propTypes));
-  return /*#__PURE__*/ React.createElement(Bar, _extends({}, barProps));
+  return /*#__PURE__*/ React.createElement(Bar, barProps);
 }
