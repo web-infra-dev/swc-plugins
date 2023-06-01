8f7254cf88c1c80013a7e45c6a362be832f252f8
neverland <jait.chen@foxmail.com>
2023-06-01 12:06:11 +0800

    fix(plugin_lodash): invalid local name (#126)
    * fix(plugin-lodash): invalid local name
    
    * chore: add changeset

---
+++ crates/plugin_lodash/tests/fixtures/mixed-fixtures/lodash-esmodule/expected.js
@@ -0,0 +1,12 @@
+import _map3 from "lodash-es/map";
+import _map2 from "lodash/map";
+import addEs from "lodash-es/add";
+import add from "lodash/add";
+add(1, 2);
+_map2([
+1
+], function() {});
+addEs(1, 2);
+_map3([
+1
+], function() {});
